27a886a8422292d70014453387b992de02f6bf74
yfarhod <49104417+yfarhod@users.noreply.github.com>
2019-11-20 23:52:11 +0100

    Updates

---
+++ precache-manifest.cc3001c0f593a9078b4e909d30a2370c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069507fef395fcd313f52c6bb6bef265",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ef533e0f210950f18526",
+    "url": "/robofriends/static/css/main.141e7eaa.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "ef533e0f210950f18526",
+    "url": "/robofriends/static/js/main.41c8cb53.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
